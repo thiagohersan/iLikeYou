819d69b7b9c17fc06c6550826dddcc55eda83dc5
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-12-20 05:08:03 -0800

    changes location of saved files

--- cgi-bin/canvas-save.php
+++ cgi-bin/canvas-save.php
@@ -1,6 +1,6 @@
 <?php
 	// requires php5
-	define('UPLOAD_DIR', 'savedImages/');
+	define('UPLOAD_DIR', '../savedImages/');
 	$img = $_POST['img'];
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
