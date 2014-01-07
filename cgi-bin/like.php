43870171fbf3c199beeef2620851cd2d820c26bf
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-01-07 01:45:15 -0800

    switches between client and server when doing the like calls

--- cgi-bin/like.php
+++ cgi-bin/like.php
@@ -2,12 +2,18 @@
 	session_start();
 	require_once('php-sdk/facebook.php');
 
-	$configs = $_SESSION['configs'];
-	$likeId = (string)$_POST['id'];
-	$appIndex = ($_POST['index'])%(count($configs));
+	if($_POST['index'] < 0){
+		$config = $_SESSION['config'];
+	}
+	else{
+		$configs = $_SESSION['configs'];
+		$appIndex = ($_POST['index'])%(count($configs));
+		$config = $configs[$appIndex];
+	}
 
+	$likeId = (string)$_POST['id'];
 	try {
-		$facebook = new Facebook($configs[$appIndex]);
+		$facebook = new Facebook($config);
 		$apiRet = $facebook->api('/'.$likeId.'/likes', 'post');
 	} catch (Exception $e) {
 		$apiRet = array('error' => array('message' => 'from php: '.$e->getMessage()));
